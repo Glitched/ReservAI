7c17c37261b8d37167b5b4f3fe8d84a73b8db858
Ryan Slama <Glitched@users.noreply.github.com>
2023-05-29 14:55:50 -0400

    Hotfix front page. This is embarrassing! (#13)
    I can't code and I can't spell, either!
    
    This removes the route that was preventing us from getting to the static
    build

--- src/main/main.py
+++ src/main/main.py
@@ -91,12 +91,6 @@ app.include_router(
 )
 
 
-@app.get("/")
-def read_root():
-    """Let's just prove the server works."""
-    return {"message": "The server is running."}
-
-
 @app.get("/auth")
 def read_auth(user: User = Depends(current_active_user)):
     """Let's just prove auth works."""
